5f8e339f5dc2a950b9fcdc9be03c8f246bcdd2b6
ShreejanaLama <shreejanaghalanb@gmail.com>
2025-04-02 16:31:57 -0400

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,23 @@
+-- create the table for artist. just saving the name
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
 
+-- albums. each album belong to 1 artist
+CREATE TABLE albums (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    artist_id INTEGER NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+
+-- songs table. includes track number and length in secs
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    album_id INTEGER NOT NULL,
+    track_number INTEGER,
+    length_seconds INTEGER,
+    FOREIGN KEY (album_id) REFERENCES albums(id)
+);
